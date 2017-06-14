ae0a1a8a72cb6f5ac36569e4fc0617c16a7ecd6a
George-Gate <terrorist_1992@hotmail.com>
2017-06-14 23:30:05 +0800

    更新matStat()
    添加了两项功能：
    1. 指定矩阵的名称
    2. 输出平均每行有多少个非零元

--- matStat.m
+++ matStat.m
@@ -1,12 +1,16 @@
-function [ varargout ] = matStat( A )
+function [ varargout ] = matStat( A, name )
 %% Display the number of non-zero elements in a Matrix
-%  [George-Gate @2017-05-06]
+%  [George-Gate @2017-06-14]
 %  [Usage]
 %       matDensity=matStat( A )
 %   
-
-    matDensity=nnz(A)/numel(A);
-    disp(['Number of non-zero elements in ',inputname(1),': ',num2str(nnz(A)),',  ',num2str(100*matDensity),'%']);
+    if nargin==1
+        name=inputname(1);
+    end
+    nnzA=nnz(A);
+    matDensity=nnzA/numel(A);
+    disp(['Number of non-zero elements in ',name,': ',num2str(nnzA),',  ',num2str(100*matDensity,'%.2f'),'%'...
+         ,',  ',num2str(nnzA/size(A,1),'%.1f'),' per row on average.']);
     if nargout ==1
         varargout{1}=matDensity;
     end
